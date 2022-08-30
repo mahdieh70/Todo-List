b72936cd44b624cb750df61f3a520732dca2d43e
mahdieh <mahdieh.mahmoodi70@gmail.com>
2022-08-30 18:59:41 +0430

    save tasks to localstorage

--- script.js
+++ script.js
@@ -4,22 +4,50 @@ const addButton = document.querySelector(".add");
 const taskList = document.querySelector(".taskList");
 const inputElem = document.querySelector("input");
 
+let tasks;
+if (!localStorage.getItem("todo")) {
+  tasks = [];
+} else {
+  tasks = getTasks();
+}
+
 //Create Li Tag
 function createTask(text) {
   let li = document.createElement("li");
   li.textContent = text;
+  li.innerHTML += ` <span class="closeBtn"><i class="fa-solid fa-trash-can"></i></span>`;
+  taskList.appendChild(li);
   return li;
 }
 
-//Insert input value to li textContent then append li to ul 
+//save tasks in localStorage
+function saveTasks(text) {
+  tasks.push(text);
+  localStorage.setItem("todo", tasks);
+}
+
+//get tasks from localStorage
+function getTasks() {
+  return localStorage.getItem("todo").split(",");
+}
+
+//show localStorage items in ul
+function showTasks() {
+  for (let taskText of getTasks()) {
+    createTask(taskText);
+  }
+}
+
+window.addEventListener("load", showTasks);
+
+//Insert input value to li textContent then append li to ul
 addButton.addEventListener("click", () => {
   let inputValue = inputElem.value;
   if (inputValue === "") {
     alert("Please insert a task!");
   } else {
-    let task = createTask(inputValue);
-    task.innerHTML += ` <span class="closeBtn"><i class="fa-solid fa-trash-can"></i></span>`;
-    taskList.appendChild(task);
+    createTask(inputValue);
+    saveTasks(inputValue);
     inputElem.value = "";
   }
 });
@@ -27,7 +55,10 @@ addButton.addEventListener("click", () => {
 //remove task or change the status of task to done
 taskList.addEventListener("click", (e) => {
   if (e.target.nodeName === "I") {
-    e.target.parentElement.parentElement.style = "display:none";
+    let target = e.target.parentElement.parentElement;
+    target.style = "display:none";
+    tasks.splice(tasks.indexOf(target.textContent), 1);
+    localStorage.setItem("todo", tasks);
   }
   if (e.target.nodeName === "LI") {
     e.target.classList.toggle("done");
